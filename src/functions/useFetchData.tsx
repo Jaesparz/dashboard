f6655e9d7078dd9694f9f5268ac87be97740d18e
Jaesparz <jaesparz@espol.edu.ec>
2025-12-14 01:27:28 +0000

    render según opcion

--- src/functions/useFetchData.tsx
+++ src/functions/useFetchData.tsx
@@ -1,16 +1,32 @@
 import { useEffect, useState } from 'react';
 import { type OpenMeteoResponse } from '../types/DashboardTypes';
 
-export default function useFetchData() {
+// Estrategia para convertir la opción seleccionada en un objeto
+const CITY_COORDS: Record<string, { latitude: number; longitude: number }> = {
+    'guayaquil': { latitude: -2.1962, longitude: -79.8862 },
+    'quito': { latitude: -0.2298, longitude: -78.525 },
+    'manta': { latitude: -0.9494, longitude: -80.7314 },
+    'cuenca': { latitude: -2.9005, longitude: -79.0045 }
 
 
-    const URL = 'https://api.open-meteo.com/v1/forecast?latitude=-3.2586&longitude=-79.9605&hourly=temperature_2m,wind_speed_10m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m';
+};
 
+// Tipo del prop: string | null
+export default function useFetchData(selectedOption: string | null) {
+
+
+   
     const [data, setData] = useState<OpenMeteoResponse>();
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
+
+         // Parametrice la opción seleccionada en la URL del requerimiento asíncrono
+    const cityConfig = selectedOption != null? CITY_COORDS[selectedOption] : CITY_COORDS["guayaquil"];
+
+    const URL = `https://api.open-meteo.com/v1/forecast?latitude=${cityConfig.latitude}&longitude=${cityConfig.longitude}&hourly=temperature_2m,wind_speed_10m&current=temperature_2m,relative_humidity_2m,apparent_temperature,wind_speed_10m`;
+
 
         const fetchData = async () => {
 
@@ -48,7 +64,7 @@ export default function useFetchData() {
 
 
 
-    }, []);
+    }, [selectedOption]);
 
     return { data, loading, error };
 
